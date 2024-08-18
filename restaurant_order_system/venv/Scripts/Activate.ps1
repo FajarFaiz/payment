571479a84329e653ad71087eb411a157b1ebde6c
Anthony <anuoluwaotoibhi@gmail.com>
2024-08-18 07:52:38 +0100

    Level 3

--- restaurant_order_system/venv/Scripts/Activate.ps1
+++ restaurant_order_system/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\New\restaurant_order_system\venv"
+$env:VIRTUAL_ENV="C:\Users\New\payment\restaurant_order_system\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- restaurant_order_system/venv/Scripts/activate.bat
+++ restaurant_order_system/venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\New\restaurant_order_system\venv"
+set "VIRTUAL_ENV=C:\Users\New\payment\restaurant_order_system\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
